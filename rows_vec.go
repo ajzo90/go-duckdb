21827026f5611144eed039f84c324d48cf5a3514
Christian <christian@infobaleen.com>
2023-04-20 20:33:34 +0200

    add BoolVec, DateVec, TsVec, U8Vec, I64Vec

--- rows_vec.go
+++ rows_vec.go
@@ -6,6 +6,7 @@ package duckdb
 import "C"
 import (
 	"io"
+	"time"
 	"unsafe"
 )
 
@@ -28,15 +29,32 @@ func (r *rows) LoadVec() error {
 }
 
 type VecScanner interface {
+	U8Vec(colIdx int) ([]uint8, error)
 	U32Vec(colIdx int) ([]uint32, error)
+	I64Vec(colIdx int) ([]int64, error)
 	F32Vec(colIdx int) ([]float32, error)
 	F64Vec(colIdx int) ([]float64, error)
 	StringVec(colIdx int, buf [][]byte) ([][]byte, error)
+	BoolVec(colIdx int) ([]bool, error)
+	TsVec(colIdx int, buf []time.Time) ([]time.Time, error)
+	DateVec(colIdx int, buf []time.Time) ([]time.Time, error)
 	LoadVec() error
 }
 
 func (r *rows) U32Vec(colIdx int) ([]uint32, error) {
 	return getGen[uint32](C.DUCKDB_TYPE_UINTEGER, r, colIdx)
+}
+
+func (r *rows) U8Vec(colIdx int) ([]uint8, error) {
+	return getGen[uint8](C.DUCKDB_TYPE_UTINYINT, r, colIdx)
+}
+
+func (r *rows) BoolVec(colIdx int) ([]bool, error) {
+	return getGen[bool](C.DUCKDB_TYPE_BOOLEAN, r, colIdx)
+}
+
+func (r *rows) I64Vec(colIdx int) ([]int64, error) {
+	return getGen[int64](C.DUCKDB_TYPE_BIGINT, r, colIdx)
 }
 
 func (r *rows) F32Vec(colIdx int) ([]float32, error) {
@@ -77,3 +95,46 @@ func getGen[T any](typ C.duckdb_type, r *rows, colIdx int) ([]T, error) {
 		return nil, errInvalidType
 	}
 }
+
+func (r *rows) TsVec(colIdx int, buf []time.Time) ([]time.Time, error) {
+	dates, err := getGen[C.duckdb_timestamp](C.DUCKDB_TYPE_TIMESTAMP_S, r, colIdx)
+	if err != nil {
+		return nil, err
+	}
+	for _, v := range dates {
+		buf = append(buf, time.Unix(int64(v.micros), 0).UTC())
+	}
+	return buf, nil
+}
+
+func (r *rows) DateVec(colIdx int, buf []time.Time) ([]time.Time, error) {
+
+	dates, err := getGen[C.duckdb_date](C.DUCKDB_TYPE_DATE, r, colIdx)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, v := range dates {
+		v := C.duckdb_from_date(v)
+		buf = append(buf, time.Date(int(v.year), time.Month(v.month), int(v.day), 0, 0, 0, 0, time.UTC))
+	}
+
+	return buf, nil
+}
+
+//func (r *rows) StrListVec(colIdx int, buf [][][]byte) ([][][]byte, error) {
+//	vector := C.duckdb_data_chunk_get_vector(r.chunk, C.idx_t(colIdx))
+//	data := C.duckdb_list_vector_get_child(vector)
+//	entries := getVec[duckdb_list_entry_t](vector)[:r.chunkRowCount]
+//
+//	for _, entry := range entries{
+//		converted := make([]any, 0, entry.length)
+//		for i := entry.offset; i < entry.offset+entry.length; i++ {
+//			value, err := scan(data, i)
+//			if err != nil {
+//				return nil, err
+//			}
+//			converted = append(converted, value)
+//		}
+//	}
+//}
